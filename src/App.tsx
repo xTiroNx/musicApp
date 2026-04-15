6c1424a7d5fa70ef8c70126548cd71c94d247775
tiron <ole_g92@bk.ru>
2026-04-15 20:14:21 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+
+export function App() {
+  const tracks = [
+    { id: 1, title: 'Song 1', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3' },
+    { id: 2, title: 'Song 2', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' },
+    { id: 3, title: 'Song 3', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' },
+    { id: 4, title: 'Song 4', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' },
+    { id: 5, title: 'Song 5', src: 'https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3' }
+  ];
+
+  return (
+    <>
+      <h1>Music player</h1>
+      <ul>
+        {
+          tracks.map((track) => {
+            return (
+              <li key={track.id}>
+                <h4>{track.title}</h4>
+                <audio controls src={track.src}></audio>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </>
+  )
+}
